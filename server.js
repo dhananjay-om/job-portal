dc922e9f9cb0d4456f4b2f9231d3bf68b4f51df0
Dhananjay Yadav <54275624+dhananjay-om@users.noreply.github.com>
2024-08-05 07:57:57 +0530

    update swagger url

--- server.js
+++ server.js
@@ -42,8 +42,8 @@ const options = {
     },
     servers: [
       {
-      url: "http://localhost:8090",
-            //url: "https://nodejs-job-portal-app.onrender.com"
+     // url: "http://localhost:8090",
+            url: "https://nodejs-job-portal-app.onrender.com"
       },
     ],
   },
